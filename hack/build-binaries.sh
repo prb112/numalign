386669a9e4e576d6dd0c0f39a2b02e8d1a03184f
Paul Bastide <paul@bastide.org>
2022-10-13 14:29:39 -0400

    Update build-binaries.sh

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -9,18 +9,25 @@ export GO111MODULE=on
 export GOPROXY=off
 export GOFLAGS=-mod=vendor
 export GOOS=linux
-export GOARCH=amd64,ppc64le
+export GOARCH=amd64
 export CGO_ENABLED=0
-
-if [ -n "${TAG}" ]; then
-	suffix="-v${TAG}-linux-amd4"
-fi
 
 for name in $( ls "${BASEDIR}/cmd/" ); do
 	path="${BASEDIR}/cmd/${name}"
 	if [ ! -d "${path}" ]; then
 		continue
 	fi
+	
+	if [ -n "${TAG}" ]; then
+		suffix="-v${TAG}-linux-amd4"
+	fi
 	echo "go build -v -o \"${BASEDIR}/_output/${name}\" \"${path}\""
+	go build -v -o "${BASEDIR}/_output/${name}${suffix}" "${path}"
+	
+	# PowerPC Build
+	if [ -n "${TAG}" ]; then
+		suffix="-v${TAG}-linux-ppc64le"
+	fi
+	export GOARCH=ppc64le
 	go build -v -o "${BASEDIR}/_output/${name}${suffix}" "${path}"
 done
